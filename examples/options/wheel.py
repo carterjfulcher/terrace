677016ca99b0285cd29adac6e30ed0078eee2a4a
Carter Fulcher <carter@fulchereng.com>
2023-06-14 20:57:15 -0600

    add examples

---
+++ examples/options/wheel.py
@@ -0,0 +1,22 @@
+from terrace import Strategy
+from terrace.data import Context, Polygon
+from terrace.trading import Engine 
+from datetime import timedelta
+
+class WheelStrategy(Strategy):
+  def step(self, ctx: Context):
+    closest_friday_date = ctx.time + timedelta(days=4 - ctx.time.weekday())
+    options = ctx.polygon.get_options("BIBL", "call", closest_friday_date.strftime("%Y-%m-%d"))
+    print(options)
+    for index, row in options.iterrows():
+      price = ctx.polygon.get_options_daily_open_close(row['ticker'], closest_friday_date.strftime("%Y-%m-%d"))
+      print(price)
+      break
+
+
+
+
+if __name__ == "__main__":
+  wheel = WheelStrategy()
+  context = Context(Polygon())
+  Engine(wheel, context).run()
--- tests/test_core.py
+++ tests/test_core.py
@@ -8,12 +8,8 @@ def test_context():
 def test_polygon():
     from terrace.data import Context
     from terrace.data import Polygon
-
     polygon = Polygon("test")
-    print(polygon.fields)
     context = Context(polygon)
-    print(context.providers)
-    print(context)
 
 if __name__ == "__main__":
     test_polygon()
